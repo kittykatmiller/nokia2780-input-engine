8b7e69974405ac3dd99af82019597759184e6de7
Kat Miller <miller.kat.kitty@gmail.com>
2026-06-04 10:55:49 -0700

    Compressed flags into one variable so hopefully it will use a marginally less amount of memory

--- include/Backlight.h
+++ include/Backlight.h
@@ -17,9 +17,10 @@ public:
 private:
 	void Timer();
 
-	std::atomic<bool> mKillFlag = false;
-	std::atomic<bool> mPokeFlag = false;
-	std::atomic<bool> mPowerFlag = false;
+	static constexpr unsigned char mKillFlag = (1u << 7); // 10000000
+	static constexpr unsigned char mPowerFlag = (1u << 0); // 00000001
+
+	std::atomic<unsigned char> mFlags = 0;
 
 	std::atomic<std::chrono::steady_clock::time_point> mStartTime = std::chrono::steady_clock::now();
 
--- src/Backlight.cpp
+++ src/Backlight.cpp
@@ -12,7 +12,7 @@ BacklightTimer::~BacklightTimer()
 	{
 		if (mTimer->joinable())
 		{
-			mKillFlag = true;
+			mFlags |= mKillFlag; // Raise kill flag (7)
 			mTimer->join();
 		}
 		delete mTimer;
@@ -24,48 +24,49 @@ void BacklightTimer::Poke()
 {
 	system("echo 1 > /sys/class/leds/white:kbd_backlight/brightness");
 	mStartTime = std::chrono::steady_clock::now();
-	mPowerFlag = true;
+	mFlags |= mPowerFlag; // Raise power flag
 }
 
 void BacklightTimer::Timer()
 {
-	while (!mKillFlag)
+	while (!(mFlags & mKillFlag))
 	{
-		if (mPowerFlag && std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - mStartTime.load()).count() >= 30000)
+		if ((mFlags & mPowerFlag) && std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - mStartTime.load()).count() >= 30000)
 		{
 			system("echo 0 > /sys/class/leds/white:kbd_backlight/brightness");
-			mPowerFlag = false;
+			mFlags &= static_cast<unsigned char>(~mPowerFlag); // Lower power flag (0)
 		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
 
-	mKillFlag = false;
+	mFlags &= static_cast<unsigned char>(~mKillFlag); // Lower kill flag (7)
 }
 
 void BacklightTimer::ForceOn()
 {
 	if (mTimer && mTimer->joinable())
 	{
-		mKillFlag = true;
+		mFlags |= mKillFlag; // Raise kill flag
 		mTimer->join();
 		delete mTimer;
 		mTimer = nullptr;
 	}
 
-	system("echo 1 > /sys/class/leds/white:kbd_backlight/brightness");
+	Poke();
 }
 
 void BacklightTimer::ForceOff()
 {
 	if (mTimer && mTimer->joinable())
 	{
-		mKillFlag = true;
+		mFlags |= mKillFlag; // Raise kill flag
 		mTimer->join();
 		delete mTimer;
 		mTimer = nullptr;
 	}
 
 	system("echo 0 > /sys/class/leds/white:kbd_backlight/brightness");
+	mFlags &= static_cast<unsigned char>(~mPowerFlag); // Lower power flag
 }
 
 void BacklightTimer::Unforce()
